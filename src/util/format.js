f3b2852cc92e3148b303d756245dc6d71d6c9671
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-08-07 16:44:27 -0400

    improve toGradient (#20)
    - change toGradient to accept value/color pairs

--- src/util/format.js
+++ src/util/format.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Color from 'color';
 
 // get html of number in exponential form
 export function toExponential(number) {
@@ -34,72 +35,46 @@ export function toComma(number) {
 }
 
 // map number to css color based on specified gradient
-// gradient should be an array of rgba() CSS color strings
-export function toGradient(number, min, max, gradient) {
+// gradient should be in format [ ... , [number, 'rgba()'], ... ]
+// values between gradient steps are linearly interpolated
+export function toGradient(number, gradient) {
   // check inputs
-  if (typeof number !== 'number')
+  if (
+    typeof number !== 'number' ||
+    !Array.isArray(gradient) ||
+    !gradient.length
+  )
     return 'rgba(255, 255, 255, 0)';
-  if (!Array.isArray(gradient) || !gradient.length)
-    gradient = ['rgba(255, 255, 255, 0)'];
-  if (typeof min !== 'number')
-    min = 0;
-  if (typeof max !== 'number')
-    max = 100;
-  if (min > max) {
-    const temp = max;
-    max = min;
-    min = temp;
+
+  // sort gradient by number
+  gradient.sort((a, b) => a[0] - b[0]);
+
+  // if provided number is outside range of gradient, return boundary color
+  if (number < gradient[0][0])
+    return gradient[0][1];
+  if (number > gradient[gradient.length - 1][0])
+    return gradient[gradient.length][1];
+
+  // find gradient entry below and above provided number
+  let lowerIndex = 0;
+  let upperIndex = gradient.length - 1;
+  for (let index = 0; index < gradient.length - 1; index++) {
+    if (gradient[index][0] <= number && gradient[index + 1][0] > number) {
+      lowerIndex = index;
+      upperIndex = index + 1;
+      break;
+    }
   }
-  if (number < min)
-    return gradient[0];
-  if (number > max)
-    return gradient[gradient.length - 1];
 
-  // get percent that number is through range
-  let percent = 0;
-  if (max - min !== 0)
-    percent = Math.abs((number - min) / (max - min));
-  percent = Math.min(Math.max(0, percent), 1);
+  // get number and color below and above provided number
+  const lowerNumber = gradient[lowerIndex][0];
+  const lowerColor = Color(gradient[lowerIndex][1]);
+  const upperNumber = gradient[upperIndex][0];
+  const upperColor = Color(gradient[upperIndex][1]);
 
-  // split each gradient color into component rgba values
-  gradient = gradient.map((color) => {
-    color = String(color)
-      .split(/[^0-9,.]/)
-      .join('');
-    color = {
-      r: parseInt(color.split(',')[0] || 255),
-      g: parseInt(color.split(',')[1] || 255),
-      b: parseInt(color.split(',')[2] || 255),
-      a: parseFloat(color.split(',')[3] || 0)
-    };
-    return color;
-  });
-
-  // map percent to float gradient index
-  const gradientIndex = (gradient.length - 1) * percent;
-  // get integer indices below/above float index
-  const lowerColor = gradient[Math.floor(gradientIndex)];
-  const higherColor = gradient[Math.ceil(gradientIndex)];
-  // get percent that float index is between nearest integer indices
-  const percentBetween = gradientIndex % 1;
-
-  // interpolate color between gradient colors below/above float index
-  let color = {
-    r: lowerColor.r + (higherColor.r - lowerColor.r) * percentBetween,
-    g: lowerColor.g + (higherColor.g - lowerColor.g) * percentBetween,
-    b: lowerColor.b + (higherColor.b - lowerColor.b) * percentBetween,
-    a: lowerColor.a + (higherColor.a - lowerColor.a) * percentBetween
-  };
-
-  // clean rgba values
-  color.r = Math.floor(color.r);
-  color.g = Math.floor(color.g);
-  color.b = Math.floor(color.b);
-  color.a = color.a.toFixed(3);
-
-  // convert color in rgba format to css color string
-  color =
-    'rgba(' + color.r + ', ' + color.g + ', ' + color.b + ', ' + color.a + ')';
+  // interpolate between below and above colors
+  const percent = (number - lowerNumber) / (upperNumber - lowerNumber);
+  const color = lowerColor.mix(upperColor, percent);
 
   // return color
   return color || 'rgba(255, 255, 255, 0)';
