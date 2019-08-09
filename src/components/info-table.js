90b0ddee933ce339e6543956eaded4dc85fd8c5f
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-08-09 14:02:17 -0400

    update

--- src/components/info-table.js
+++ src/components/info-table.js
@@ -11,7 +11,7 @@ export class InfoTable extends Component {
   render() {
     const rows = this.props.bodyContents.map((row, index) => (
       <React.Fragment key={index}>
-        <Tooltip text={row[1] || ''}>
+        <Tooltip text={row[1]}>
           <div className='info_table_key small left light semibold'>
             {row[0]}
           </div>
